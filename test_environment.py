b84ea4f9cb6cf9f6a49486ee87673680b5dad28b
Luke Thayer <lthayer.freelance@gmail.com>
2023-12-02 09:21:39 -0800

    More python exercises

--- test_environment.py
+++ test_environment.py
@@ -1,8 +1,18 @@
+#4 Iterate Through a Dictionary with List Values
+#Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
 
-person = {"first": "Ada", "last": "Lovelace", "age": 42, "is_organ_donor": True}
-# Adds a new key value pair for email to person
-person["email"] = "alovelace@codingdojo.com"
-        
-# Changes person's "last" value to "Bobada"
-person["last"] = "Bobada"
-print(person)
+def printInfo(some_dictionary):
+    for dic_count in dojo:
+        x = str(dic_count).upper()
+        print(len(some_dictionary[dic_count]), x)
+        for list_count in some_dictionary[dic_count]:
+            print(list_count)
+        print(" ")
+
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
